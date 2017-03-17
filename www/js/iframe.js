5125857763ec50fed23fdfa953e1e444ffd0db28
korybartlett <korybart@gmail.com>
2017-03-17 14:44:47 -0700

    queue and search working together

--- www/js/iframe.js
+++ www/js/iframe.js
@@ -20,15 +20,15 @@ $(document).ready(function() {
                 
                 console.log(item.snippet.thumbnails.default.url);
                 var listItem = [
-                    '<li><div class="searchResults-ul-img">',
-                    '<img src="'+item.snippet.thumbnails.default.url+'"> <!-- the thumbnail --></div>',
+                    '<li id="'+vidId+'" class="searchResults-item"><div class="searchResults-ul-img" >',
+                    '<img src=" '+item.snippet.thumbnails.default.url+' " onclick="moveToQueue(\''+vidId+'\');"> <!-- the thumbnail --></div>',
                     '<div class="searchResults-ul-li">',
                     '<h6>Team A - 2</h6> <!-- Team 1 and score -->',
                     '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
                     '<h6>1/21/2017</h6> <!-- Date --></div></li>'
                 ];
         
-                 $(".searchResults-ul").append(listItem.join(''));
+                $(".searchResults-ul").append(listItem.join(''));
 
     		})
         }
@@ -37,5 +37,47 @@ $(document).ready(function() {
 });
 
 
-
 console.log(vidIdList);
+
+function moveToQueue(vidId) {
+    var movId;
+    $.get(
+        "https://www.googleapis.com/youtube/v3/videos", {
+            part: 'snippet',
+            id: vidId,
+            maxResults: 1,
+            key: 'AIzaSyDRIWeEmYpopkQBrLH7uthr4YPJU8XxfuA'
+        },
+        //function traverses through received items
+        function(data) {
+            var outputs;
+            $.each(data.items, function(i, item) {
+                console.log(item);
+                //saves the video ids to the array
+                movId = item.id;
+                //console.log(movId)
+               // vidIdList.push(vidId);
+                
+                //console.log(item.snippet.thumbnails.default.url);
+                var listItem = [
+                    '<li id="'+movId+'" class="theQueue-item"><div class="theQueue-ul-img" >',
+                    '<img src=" '+item.snippet.thumbnails.default.url+' " onclick="moveToQueue(\''+movId+'\');"> <!-- the thumbnail --></div>',
+                    '<div class="theQueue-ul-li">',
+                    '<h6>Team A - 2</h6> <!-- Team 1 and score -->',
+                    '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
+                    '<h6>1/21/2017</h6> <!-- Date --></div></li>'
+                ];
+        
+                //$(".theQueue-ul").prepend(listItem.join(''));
+
+            })
+        }
+    ); 
+    //find the current li we are at
+    //save the data from the list item
+    console.log(vidId);
+    //$('#'+vidId).clone(true).inserAfter('theQueue-ul');
+    $('.theQueue-ul').prepend($('#'+vidId).clone());
+    
+    $('#'+vidId+'.searchResults-item').last().remove();
+}
