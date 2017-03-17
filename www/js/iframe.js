f67ea9b3f14554a592a90740255b514615feeb30
korybartlett <korybart@gmail.com>
2017-03-17 16:34:08 -0700

    commented JS file dealing with iframe, upadted iframe to be a youtube player on index

--- www/js/iframe.js
+++ www/js/iframe.js
@@ -16,9 +16,10 @@ $(document).ready(function() {
                 console.log(item);
                 //saves the video ids to the array
                 var vidId = item.snippet.resourceId.videoId;
+                //pushes video ids to an array
                 vidIdList.push(vidId);
                 
-                console.log(item.snippet.thumbnails.default.url);
+                //console.log(item.snippet.thumbnails.default.url);
                 var listItem = [
                     '<li id="'+vidId+'" class="searchResults-item"><div class="searchResults-ul-img" >',
                     '<img src=" '+item.snippet.thumbnails.default.url+' " onclick="moveToQueue(\''+vidId+'\');"> <!-- the thumbnail --></div>',
@@ -27,7 +28,8 @@ $(document).ready(function() {
                     '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
                     '<h6>1/21/2017</h6> <!-- Date --></div></li>'
                 ];
-        
+                
+                //appends the items to the search list 
                 $(".searchResults-ul").append(listItem.join(''));
 
     		})
@@ -41,6 +43,7 @@ console.log(vidIdList);
 
 function moveToQueue(vidId) {
     var movId;
+    //ajax calss to youtube videos
     $.get(
         "https://www.googleapis.com/youtube/v3/videos", {
             part: 'snippet',
@@ -67,34 +70,32 @@ function moveToQueue(vidId) {
                     '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
                     '<h6>1/21/2017</h6> <!-- Date --></div></li>'
                 ];
-        
-                //$(".theQueue-ul").prepend(listItem.join(''));
-
             })
         }
     ); 
-    //find the current li we are at
-    //save the data from the list item
     //console.log(vidId);
-    //$('#'+vidId).clone(true).inserAfter('theQueue-ul');
+    //adds to the top of the Queue
     $('.theQueue-ul').prepend($('#'+vidId).clone());
-    
+    //removes the last occurence, which is hopefully in the search result 
     $('#'+vidId+'.searchResults-item').last().remove();
 }
 
 //This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+//funcion to create youtbue iFrame player
 var player;
 function onYouTubeIframeAPIReady() {
+    //creates player which replaces div with id=player
     player = new YT.Player('player', {
       height: '400',
       width: '650',
+      //creates player with hardcoded ID for now
       videoId: 'jLRFACyYE6Y',
+      //creates events to use with youtube player
       events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange
@@ -102,6 +103,7 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
+//function for onPlayerReady, plays video
 function onPlayerReady(event) {
         event.target.playVideo();
 }
@@ -113,10 +115,13 @@ function onPlayerReady(event) {
     }
 }
 
+//function to move videos from queue to the player
 function queueToPlayer(){
-    //player.loadVideoById("qRIFuIMqe94");
+    //grabs the ID attribute from the first element in the queue
     var queueId = $('.theQueue-ul li:first-child').attr('id');
+    //removes the first element from the queue
     $('.theQueue-ul li:first-child').remove();
     //console.log(queueId);
+    //loads video to video player with video ID
     player.loadVideoById(queueId);
 }
