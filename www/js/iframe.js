e75dd23c4e4220e725abac447ea4f8a6ad414f37
korybartlett <korybart@gmail.com>
2017-04-02 14:06:15 -0700

    jquery grabs the html text of queue when searching by ID

--- www/js/iframe.js
+++ www/js/iframe.js
@@ -1,8 +1,9 @@
-var playlistId = 'PL31NN8Vu_FZ_ZEABi73pW_0YXqbOJ_76-';
+var playlistId = 'PLIqH-UbFXUsWh_d_tplAaGFUgbBwXE6k7';
 var vidIdList = [];
 
 $(document).ready(function () {
     //get request to get youtube playlist
+    var score = 0;
     $.get(
         "https://www.googleapis.com/youtube/v3/playlistItems",  
         {
@@ -19,14 +20,17 @@ $(document).ready(function () {
                 var vidId = item.snippet.resourceId.videoId;
                 //pushes video ids to an array
                 vidIdList.push(vidId);
-                
+                var aScore, bScore;
+                score++;
+                aScore = score;
+                bScore = 10 - score;
                 //console.log(item.snippet.thumbnails.default.url);
                 var listItem = [
                     '<li id="'+vidId+'" class="searchResults-item"><div class="searchResults-ul-img" >',
                     '<img src=" '+item.snippet.thumbnails.default.url+' " onclick="moveToQueue(\''+vidId+'\');"> <!-- the thumbnail --></div>',
                     '<div class="searchResults-ul-li">',
-                    '<h6>Team A - 2</h6> <!-- Team 1 and score -->',
-                    '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
+                    '<h6>Team A - '+aScore+' </h6> <!-- Team 1 and score -->',
+                    '<h6>Team B - '+bScore+' </h6> <!-- Team 2 and score -->',
                     '<h6>1/21/2017</h6> <!-- Date --></div></li>'
                 ];
                 
@@ -44,7 +48,7 @@ console.log(vidIdList);
 
 function moveToQueue(vidId) {
     var movId;
-    //ajax calss to youtube videos
+    //ajax call to youtube videos
     $.get(
         "https://www.googleapis.com/youtube/v3/videos", {
             part: 'snippet',
@@ -58,27 +62,28 @@ function moveToQueue(vidId) {
             $.each(data.items, function(i, item) {
                 console.log(item);
                 //saves the video ids to the array
+                //saves the video id to a variable for later use
                 movId = item.id;
                 //console.log(movId)
                // vidIdList.push(vidId);
-                
-                //console.log(item.snippet.thumbnails.default.url);
-                var listItem = [
-                    '<li id="'+movId+'" class="theQueue-item"><div class="theQueue-ul-img" >',
-                    '<img src=" '+item.snippet.thumbnails.default.url+' " onclick="moveToQueue(\''+movId+'\');"> <!-- the thumbnail --></div>',
-                    '<div class="theQueue-ul-li">',
-                    '<h6>Team A - 2</h6> <!-- Team 1 and score -->',
-                    '<h6>Team B - 1</h6> <!-- Team 2 and score -->',
-                    '<h6>1/21/2017</h6> <!-- Date --></div></li>'
-                ];
             })
         }
     ); 
     //console.log(vidId);
+
+    //clones the searchResult video item to the queue, copies the entire item data
     //adds to the top of the Queue
     $('.theQueue-ul').prepend($('#'+vidId).clone());
+
     //removes the last occurence, which is hopefully in the search result 
     $('#' + vidId + '.searchResults-item').last().remove();
+    
+    //adjusts theQueue class attributes because of cloning
+    $('#' + vidId).removeClass('searchResults-item');
+    $('#' + vidId + ' .searchResults-ul-img').removeClass('searchResults-ul-img').addClass('theQueue-ul-img');
+    $('#' + vidId + ' .searchResults-ul-li').removeClass('searchResults-ul-li').addClass('theQueue-ul-li');
+
+
 }
 
 //This code loads the IFrame Player API code asynchronously.
@@ -95,7 +100,7 @@ function onYouTubeIframeAPIReady() {
       height: '400',
       width: '650',
       //creates player with hardcoded ID for now
-      videoId: 'jLRFACyYE6Y',
+      videoId: 'o5aYww6nf0s',
       //creates events to use with youtube player
       events: {
         'onReady': onPlayerReady,
@@ -120,9 +125,18 @@ function onPlayerReady(event) {
 function queueToPlayer(){
     //grabs the ID attribute from the first element in the queue
     var queueId = $('.theQueue-ul li:first-child').attr('id');
+    //console.log(queueId);
+
+    //function will be used to grab scores 
+    //jquery searches using the videoId, aka its queueId, then grabs only the text from the html DOM containing the score
+    var score = $('#' + queueId + ' .theQueue-ul-li').text();
+    console.log(score);
+
     //removes the first element from the queue
     $('.theQueue-ul li:first-child').remove();
-    //console.log(queueId);
+
     //loads video to video player with video ID
     player.loadVideoById(queueId);
+    $('div.currentScore-leftSide > h2').replaceWith('<h2>5</h2>');
+    $('div.currentScore-rightSide > h2').replaceWith('<h2>5</h2>');
 }
