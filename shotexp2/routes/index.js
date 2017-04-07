8c929bf74f1b46081e0e31af0ff70bd616e2c8c1
ffeyzi <fffeyzi@gmail.com>
2017-04-06 21:40:03 -0700

    the express files

---
+++ shotexp2/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page - also default pathway */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', val2: 'rory' });
+});
+
+/* GET NBA page. */
+router.get('/nba', function(req, res, next) {
+  res.render('index', { title: 'NBA',val2: 'NBA' });
+});
+
+/* GET NBA page. */
+router.get('/epl', function(req, res, next) {
+  res.render('index', { title: 'EPL', val2: 'EPL' });
+});
+
+//This is what the return value of the entire file will be.
+module.exports = router;
+
+//testing
