8ba8e2a3c4fabadfb65cdb18f87ffee75cc02191
ffeyzi <fffeyzi@gmail.com>
2017-05-10 14:47:35 -0700

    the epl is added

--- shotexp2/routes/index.js
+++ shotexp2/routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-// request('http://localhost:9200/seniordesign/nba/_search', function (error, response, body) {
-//   		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   		var matchedGames = response.body;
-//   		console.log('body:' , matchedGames);
-//   		parsedGames = JSON.parse(matchedGames);
-//   		betterParsedGames = parsedGames.hits.hits;
-//   		console.log('parsedGames: ', betterParsedGames.length);
-//   		//prints the information in the way we'd like. 
-//   		for (i=0; i<betterParsedGames.length; i++) {
-//   			console.log(betterParsedGames[i]._source);
-//   		}
-// });
+request('http://localhost:9200/seniordesign/nba/_search', function (error, response, body) {
+  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  		var matchedGames = response.body;
+  		console.log('body:' , matchedGames);
+  		parsedGames = JSON.parse(matchedGames);
+  		betterParsedGames = parsedGames.hits.hits;
+  		console.log('parsedGames: ', betterParsedGames.length);
+  		//prints the information in the way we'd like. 
+  		for (i=0; i<betterParsedGames.length; i++) {
+  			console.log(betterParsedGames[i]._source);
+  		}
+});
 
 /* GET home page - also default pathway */
 router.get('/', function(req, res, next) {
@@ -40,9 +40,14 @@ router.get('/login', function(req, res, next) {
 	res.render('login', { title: 'Login', val2: 'Customize' });
 });
 
-/* Logged In Session Page */
-router.get('/loggedin', function(req, res, next) {
-	res.render('loggedin', { title: 'Logged in Now', val2: 'here we go'})
+/* Logged In Session Page: NBA */
+router.get('/loggedinNBA', function(req, res, next) {
+	res.render('loggedinNBA', { title: 'Logged in Now', val2: 'here we go'})
+});
+
+/* Logged In Session Page: EPL */
+router.get('/loggedinEPL', function(req, res, next) {
+	res.render('loggedinEPL', { title: 'Logged in Now', val2: 'here we go'})
 });
 
 //This is what the return value of the entire file will be.
