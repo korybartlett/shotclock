cd0bf32786af82571660a64033e94e33843bae82
korybartlett <korybart@gmail.com>
2017-06-04 12:31:31 -0700

    updated soccer for complete season

--- code/test/js/epl.js
+++ code/test/js/epl.js
@@ -8,9 +8,11 @@ var titleArr = [];
 var dateArr = [];
 var check = 1;
 var checkArr = [];
+var loadedCSV = [];
 
 $(document).ready( function() {
-	getVid(PageToken);
+	initLoad();
+	$('#loadbtn2').click(runAPI);
 });
 
 function getVid(PageToken){
@@ -31,11 +33,12 @@ function getVid(PageToken){
 
 
 
-var patPrem = new RegExp("Premier League");
-var patPL = new RegExp("PL"); 
+var patPrem = new RegExp("Premier League Preview");
+//var patPL = new RegExp("PL"); 
 var patMIB = new RegExp("Men In Blazers Off [T|t]he Ball");
 var patNBC = new RegExp("NBC");
 var patTF = new RegExp('[T|t]op five');
+var patPPR = new RegExp('[P|p]layer [P|p]ower [R|r]ankings');
 var pat = new RegExp();
 
 function list(data){
@@ -46,17 +49,19 @@ function list(data){
 		sum++;
 		// console.log("The sum: "+ sum);
 		// console.log("The total " + total);
-		if(sum == total || sum == 610){
+		if(sum == total || sum == 800){
 			// var top = "<li>" + added + "</li>";
 			// console.log("done");
 			// console.log(added);
 			// $('#results').prepend(top);
-			var data = dateArr.join("\n")
+			
+			console.log(csvArr);
+			//var data = $.csv.fromArrays(csvArr);
+			var data = csvArr.join("\n")
 			var csvContent = "data:text/csv;charset=utf-8,";
 			csvContent= csvContent + data;
 			var encodedUri = encodeURI(csvContent);
-      		window.open(encodedUri);
-
+			window.open(encodedUri);
 			return;
 		}
 		//console.log(total);
@@ -65,36 +70,59 @@ function list(data){
 		date = "";
 
 		var title = data.items[i].snippet.title;
-		
+		var nTitle = title.replace(/,/g , " ");
+		var vidId = data.items[i].snippet.resourceId.videoId;
+
 		if(title === 'Private video'){
 			continue;
 		}
-		if(patPrem.test(title) || patPL.test(title) || patMIB.test(title) || patNBC.test(title) || patTF.test(title) ){
+
+		if(patPrem.test(title) || patPPR.test(title) || patMIB.test(title) || patNBC.test(title) || patTF.test(title) ){
 			continue;
 		}
 
 		var teams = getTeamNames(title);
 		if(teams === ''){
+			console.log("Skipped game title: "+title);
 			continue;
 		}
 
-		var nTitle = title.replace(/,/g , " ");
-		var vidId = data.items[i].snippet.resourceId.videoId;
-		getdate(title);
+		
+
+		//getdate(title);
 		//console.log(date);
 		titleArr.push(title);
-		outputs = teams + "," + vidId + "," + nTitle + "," + date;
+		outputs = teams + "," + vidId + "," + nTitle;// + "," + date;
 		csvArr.push(outputs);
 		outputs = "<li>" + outputs + "</li>";
 		$('#results').append(outputs);
 		added++;
 
+		// if(vidId == loadedCSV[1][4]){
+		// 	sum = 609;
+		// 	total = sum + 1;
+		// 	console.log("FOUND A MATCH");
+		// }
+
+		// if('gFQFPYMm8_M' == vidId){
+		// 	sum = 609;
+		// 	total = sum + 1;
+		// 	console.log("FOUND A BAd MATCH");
+		// }
 
 	}
 
 	if(sum < (total-1)){
 		getVid(nextPageToken);
 	}
+}
+
+function sendTitle(){
+	for(var i = 1; i < loadedCSV.length; i++){
+		getdate(loadedCSV[i][6]);
+	}
+	console.log(dateArr);
+
 }
 
 function getdate(title){
@@ -117,13 +145,21 @@ function grab(data){
 	//console.log(tempDate);
 	date = tempDate.substring(0,10);
 	dateArr.push(date);
-	console.log(dateArr);
+	//console.log(dateArr);
 	console.log(check);
 	check++;
+	console.log(check);
+	if(check == loadedCSV.length-1){
+		var data = dateArr.join("\n")
+		var csvContent = "data:text/csv;charset=utf-8,";
+		csvContent= csvContent + data;
+		var encodedUri = encodeURI(csvContent);
+		window.open(encodedUri);
+	}
 	//console.log(date);
 }
 
-var premTeam = ["Chelsea", "Tottenham", "Liverpool", "Man City", "Man United", "Manchester United", "Arsenal", "Everton", "West Brom", "Southampton", "Bournemouth", "Leicester", "Stoke", "Watford", "Burnley", "West Ham", "Crystal Palace", "Hull", "Swansea", "Middlesbrough", "Sunderland"]
+var premTeam = ["Chelsea", "Tottenham", "Liverpool", "Man City", "Man United", "Manchester United", "Arsenal", "Everton", "West Brom", "Southampton", "Bournemouth", "Leicester", "Stoke", "Watford", "Burnley", "West Ham", "Crystal Palace", "Hull", "Swansea", "Middlesbrough", "Sunderland", "Palace", "Manchester City", "Man Utd", "WBA"]
 
 function getTeamNames(title){
 	var count = 0;
@@ -132,11 +168,11 @@ function getTeamNames(title){
 	for(var j = 0; j < premTeam.length; j++){
 		if(title.includes(premTeam[j]) ){
 			count++;
-			if(j == 3){
+			if(j == 3 || j == 22){
 				teams.push("Manchester City")
 				continue;
 			}
-			if(j == 4){
+			if(j == 4 || j == 23){
 				teams.push("Manchester United");
 				continue;
 			}
@@ -156,6 +192,13 @@ function getTeamNames(title){
 				teams.push("Swansea City");
 				continue;
 			}
+			if(j == 21){
+				teams.push("Crystal Palace");
+				continue;
+			}
+			if(j == 24){
+				teams.push("West Brom");
+			}
 			teams.push(premTeam[j]);
 			//teams = teams + premTeam[j] + " ";
 		}
@@ -170,4 +213,44 @@ function getTeamNames(title){
 	else{
 		return "";
 	}
+}
+
+function csvData(){
+	if( document.getElementById("filebtn").files.length == 0 ){
+		alert("no files selected, unable to load progress report");
+		return;
+	}
+
+	//establishes file reader object
+	var fr = new FileReader();
+	//grabs file properties with uploaded files
+	var files = $("#filebtn").prop('files');
+
+	//grabs the first file object from file reader
+	fr.readAsText(files[0]);
+
+	//loads the file information from file object and splits at comma
+	fr.onload = function() {
+		var importData = this.result;
+		loadedCSV = $.csv.toArrays(importData);
+		console.log(loadedCSV);
+		// var data = $.csv.fromArrays(csvArr);
+		// var csvContent = "data:text/csv;charset=utf-8,";
+		// csvContent= csvContent + data;
+		// var encodedUri = encodeURI(csvContent);
+		// window.open(encodedUri);
+	}
+}
+
+function runAPI(){
+	//getVid(PageToken);
+	sendTitle();
+	console.log(dateArr);
+}
+
+function initLoad(){
+  //onclick functionality that grabs information from imported files
+  $("#loadbtn").click(function() {
+    csvData();
+  });
 }
--- code/test/js/eplScores.js
+++ code/test/js/eplScores.js
@@ -7,6 +7,64 @@ $(document).ready( function() {
 	initLoad();
 });
 
+function fixDate(){
+	for(var i=1; i < scoreArr.length; i++){
+		var csvDate = scoreArr[i][4];
+		var dateSplit = csvDate.split("/");
+		var month = dateSplit[0];
+
+		if(month < 10){
+			month = month.substring(1, month.length);
+			dateSplit[0] = month;
+			scoreArr[i][4] = dateSplit.join("/");
+		}
+	}
+}
+
+function addScore(){
+	var index = scoreArr.length - 1;
+	for(var i = 1; i < csvArr.length; i++){
+		for(var j = index; j > 0 ; j--){
+			var match = 0;
+			//name check for first column
+			if(csvArr[i][0] == scoreArr[j][0] && csvArr[i][2] == scoreArr[j][2]){
+				var csvArrDateSplit = csvArr[i][5].split("/");
+				var scoreArrDateSplit = scoreArr[j][4].split("/");
+				// console.log(csvArrDateSplit);
+				// console.log(scoreArrDateSplit);
+				if(csvArrDateSplit[0] == scoreArrDateSplit[0] && csvArrDateSplit[2] == scoreArrDateSplit[2]){
+					console.log("match made at " + i + " and j of " + j);
+					csvArr[i][1] = scoreArr[j][1];
+					csvArr[i][3] = scoreArr[j][3];
+					csvArr[i][5] = scoreArr[j][4];
+					match = 1;
+					// index = j;
+				}
+			}
+
+			if(csvArr[i][0] == scoreArr[j][2] && csvArr[i][2] == scoreArr[j][0]){
+				var csvArrDateSplit = csvArr[i][5].split("/");
+				var scoreArrDateSplit = scoreArr[j][4].split("/");
+				// console.log(csvArrDateSplit);
+				// console.log(scoreArrDateSplit);
+				if(csvArrDateSplit[0] == scoreArrDateSplit[0] && csvArrDateSplit[2] == scoreArrDateSplit[2]){
+					console.log("match made at " + i + " and j of " + j);
+					csvArr[i][1] = scoreArr[j][3];
+					csvArr[i][3] = scoreArr[j][1];
+					csvArr[i][5] = scoreArr[j][4];
+					match = 1;
+					// index = j;
+				}
+			}
+
+			if(match == 1){
+				break;
+			}
+		}
+	}
+	createCSV();
+}
+
 function changeCSVArr(){
 	var team1 = team2 = "";
 	
@@ -21,22 +79,6 @@ function changeCSVArr(){
 		if(match){
 			csvArr.splice(i, 1);
 		}	
-	}
-}
-
-function createCSV(){
-	for(var i = 0; i<csvArr.length; i++){
-		var count = 0;
-		for(var j = 0; j<premTeam.length; j++){
-			if(csvArr[i][0].includes(premTeam[j])){
-				count++;
-			}	
-			if(count==2){
-				var arrayStr = csvArr[i][0].replace(/\s\s+/g, ", ");
-				arrToCSV.push(arrayStr);
-				break;
-			}
-		}
 	}
 }
 
@@ -112,67 +154,76 @@ function clearWS(){
 // 	console.log("Number of matches: " + match);
 // }
 
-function getScore16(){
-	var match = 0;
-	for(var i = csvArr.length - 1; i > 156; i--){
-		var tempMatch = match;
-		var team1 = csvArr[i][0];
-		var team2 = csvArr[i][2];
-		for (var j = 2; j < scoreArr.length; j++) {
-			var teamLeft = scoreArr[j][0];
-			var teamRight = scoreArr[j][2];
+// function getScore16(){
+// 	var match = 0;
+// 	for(var i = csvArr.length - 1; i > 156; i--){
+// 		var tempMatch = match;
+// 		var team1 = csvArr[i][0];
+// 		var team2 = csvArr[i][2];
+// 		for (var j = 2; j < scoreArr.length; j++) {
+// 			var teamLeft = scoreArr[j][0];
+// 			var teamRight = scoreArr[j][2];
 
-			if(teamLeft == "" || teamRight == "" || team1 == "" || team1 == ""){
-				continue;
-			}
+// 			if(teamLeft == "" || teamRight == "" || team1 == "" || team1 == ""){
+// 				continue;
+// 			}
 
-			if(teamLeft.includes(team1)){
-				if (teamRight.includes(team2)){
-					match++;
-					// console.log("team1: " + team1);
-					// console.log("team2: " + team2);
-					// console.log("teamLeft: " + teamLeft);
-					// console.log("teamRight: " + teamRight);
-					// console.log("j:" + j);
-					// console.log("i:" + i);
-					var score = scoreArr[j][1];
-					score = score.replace(/\s+/g, '');
-					var scoreTemp = score.split("-");
-					csvArr[i][1] = scoreTemp[0];
-					csvArr[i][3] = scoreTemp[1];
-					break;
-				}
-			}
-			else if(teamRight.includes(team1)){
-				if (teamLeft.includes(team2)){
-					match++;
-					// console.log("team1: " + team1);
-					// console.log("team2: " + team2);
-					// console.log("teamLeft: " + teamLeft);
-					// console.log("teamRight: " + teamRight);
-					// console.log("j:" + j);
-					// console.log("i:" + i);
-					var score = scoreArr[j][1];
-					score = score.replace(/\s+/g, '');
-					var scoreTemp = score.split("-");
-					csvArr[i][1] = scoreTemp[1];
-					csvArr[i][3] = scoreTemp[0];
-					break;
-				}
-			}
-		}
-		if(tempMatch == match){
-			console.log("Unable to match");
-			console.log("team1: " + team1);
-			console.log("team2: " + team2);
-			console.log("");
-		}
+// 			if(teamLeft.includes(team1)){
+// 				if (teamRight.includes(team2)){
+// 					match++;
+// 					// console.log("team1: " + team1);
+// 					// console.log("team2: " + team2);
+// 					// console.log("teamLeft: " + teamLeft);
+// 					// console.log("teamRight: " + teamRight);
+// 					// console.log("j:" + j);
+// 					// console.log("i:" + i);
+// 					var score = scoreArr[j][1];
+// 					score = score.replace(/\s+/g, '');
+// 					var scoreTemp = score.split("-");
+// 					csvArr[i][1] = scoreTemp[0];
+// 					csvArr[i][3] = scoreTemp[1];
+// 					break;
+// 				}
+// 			}
+// 			else if(teamRight.includes(team1)){
+// 				if (teamLeft.includes(team2)){
+// 					match++;
+// 					// console.log("team1: " + team1);
+// 					// console.log("team2: " + team2);
+// 					// console.log("teamLeft: " + teamLeft);
+// 					// console.log("teamRight: " + teamRight);
+// 					// console.log("j:" + j);
+// 					// console.log("i:" + i);
+// 					var score = scoreArr[j][1];
+// 					score = score.replace(/\s+/g, '');
+// 					var scoreTemp = score.split("-");
+// 					csvArr[i][1] = scoreTemp[1];
+// 					csvArr[i][3] = scoreTemp[0];
+// 					break;
+// 				}
+// 			}
+// 		}
+// 		if(tempMatch == match){
+// 			console.log("Unable to match");
+// 			console.log("team1: " + team1);
+// 			console.log("team2: " + team2);
+// 			console.log("");
+// 		}
 		
-	}
-	console.log("Number of matches: " + match);
+// 	}
+// 	console.log("Number of matches: " + match);
+// }
+
+function createCSV(){
+	console.log(csvArr);
+	var data = $.csv.fromArrays(csvArr);
+	var csvContent = "data:text/csv;charset=utf-8,";
+	csvContent= csvContent + data;
+	var encodedUri = encodeURI(csvContent);
+	window.open(encodedUri);
 }
 
-function csvData(){
+function load2CSV(){
 	if( document.getElementById("filebtn").files.length == 0 ){
 		alert("no files selected, unable to load progress report");
 		return;
@@ -192,16 +243,18 @@ function csvData(){
 		//importData = importData.split(",");
 		//console.log(importData);
 		csvArr = $.csv.toArrays(importData);
+		console.log(csvArr);
 		//getArrVal();
 		//changeCSVArr();
 		//createCSV();
 		//clearWS();
-		getScore16();
-		var data = $.csv.fromArrays(csvArr);
-		var csvContent = "data:text/csv;charset=utf-8,";
-		csvContent= csvContent + data;
-		var encodedUri = encodeURI(csvContent);
-		window.open(encodedUri);
+		addScore();
+		// console.log(csvArr);
+		// var data = $.csv.fromArrays(csvArr);
+		// var csvContent = "data:text/csv;charset=utf-8,";
+		// csvContent= csvContent + data;
+		// var encodedUri = encodeURI(csvContent);
+		// window.open(encodedUri);
 	}
 }
 
@@ -218,7 +271,10 @@ function loadCSV(){
 	fr.onload = function() {
 		var importData = this.result;
 		scoreArr = $.csv.toArrays(importData);
-		csvData();
+		//csvData();
+		//fixDate()
+		load2CSV();
+		//console.log(scoreArr);
 	}
 
 	console.log("Loaded scores");
@@ -230,6 +286,6 @@ function initLoad(){
     loadCSV();
   });
   $("#loadbtn2").click(function() {
-    csvData();
+    load2CSV();
   });
 }
